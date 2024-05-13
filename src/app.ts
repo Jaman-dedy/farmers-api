3822c0b330d6b261f72a331cb055fa91178f705d
Jaman-dedy <jeandedieuam@gmail.com>
2024-05-13 21:25:13 +0200

    add the server

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { config } from 'dotenv';
+
+config();
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Farmer Ordering System API',
+      version: '1.0.0',
+      description: 'API documentation for the Farmer Ordering System',
+    },
+  },
+  apis: ['./src/routes/*.ts'], // Path to the API route files
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes
+// TODO: Import and use route files
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;
